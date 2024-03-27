caf58a6f1f044648725fb02aa7c10c2ebf39f619
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-03-27 16:21:05 +0100

    add CSV files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //make seeders for my db, not commented out
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
     }
 }
