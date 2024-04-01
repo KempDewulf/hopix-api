baab2b9fc5afac040fac5ea92abd1e85bf4b75bd
kemp.dewulf <kemp.dewulf@student.howest.be>
2024-04-01 21:26:49 +0200

    add services

--- app/Modules/Beers/Services/BeerService.php
+++ app/Modules/Beers/Services/BeerService.php
@@ -1,3 +1,26 @@
 <?php
 
+namespace App\Modules\Beers\Services;
 
+use App\Models\Beer;
+use App\Modules\Core\Services\Service;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
+
+class BeerService extends Service
+{
+    protected $rules = [
+        'name' => 'required',
+    ];
+
+    public function validate($data)
+    {
+        $this->errors = new MessageBag();
+
+        $validator = Validator::make($data, $this->rules);
+
+        if ($validator->fails()) {
+            $this->errors = $validator->errors();
+        }
+    }
+}
---
+++ app/Modules/Core/Services/Service.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Modules\Core\Services;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
+
+abstract class Service
+{
+    protected $model;
+    protected $errors;
+    protected $rules;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+        $this->errors = new MessageBag();
+    }
+
+    protected function validate($data)
+    {
+        $this->errors = new MessageBag();
+
+        $validator = Validator::make($data, $this->rules);
+
+        if ($validator->fails()) {
+            $this->errors = $validator->errors();
+        }
+    }
+
+    public function hasErrors()
+    {
+        return $this->errors->any();
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function create($data)
+    {
+        $this->validate($data);
+        if ($this->hasErrors()) {
+            return null;
+        }
+
+        return $this->model->create($data);
+    }
+
+    public function update($id, $data)
+    {
+        $this->validate($data);
+        if ($this->hasErrors()) {
+            return null;
+        }
+
+        return $this->model->find($id)->update($data);
+    }
+}
